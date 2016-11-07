8558c036fa6ddf21ed7af44ca41b820f28d34115
maximilian1064 <lei.yan@rwth-aachen.de>
2016-11-07 21:31:15 +0100

    add some comments to e2-1.c, add .ycm_extra_conf.py

--- textbook-assignments/chap-1/e2-1.c
+++ textbook-assignments/chap-1/e2-1.c
@@ -1,3 +1,20 @@
+/* 
+ * THE C PROGRAMMING LANGUAGE
+ *
+ * Exercise 2-1. Write a program to determine the ranges of char , short , int , and long
+ * variables, both signed and unsigned , by printing appropriate values from standard headers
+ * and by direct computation. Harder if you compute them: determine the ranges of the various
+ * floating-point types.
+ *
+ *
+ * Format of signed integer here, based on two's complement
+ * 0x00000000 --- 0x7FFFFFFF ---> 0 --- 2^31-1
+ * 0x80000000 --- 0xFFFFFFFF ---> -2^31 --- -1
+ * Get the length, then the range can be calculated
+ *
+ *
+ */
+
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
@@ -38,8 +55,7 @@ int main()
     printf("range of unsigned int: 0 - %lu\n", (unsigned long)((1lu<<length)-1) );
     printf("range of signed int: %ld - %ld\n", (long)(-(1<<length-1)), (long)((1<<length-1)-1) );  
 
-
-
+    
     length = 0;
     while(l_base)
     {
@@ -48,6 +64,18 @@ int main()
     }
     printf("range of unsigned long: 0 - %lu\n", (1lu<<length-1) + ((1lu<<length-1)-1) );
     printf("range of signed long: %ld - %ld\n", -(1l<<length-2) - (1l<<length-2) , (1l<<length-2) + ((1l<<length-2)-1) );  
+
+    /* In current environment(hardware, OS, compiler), constant will be assigned automatically TYPE int, 
+     * Thus expression with constant operand and with result exceeds the range of TYPE int will get a overflow,
+     * Remember to mark the constant as TYPE long first with l/lu suffix
+     * Especially important when deal with TYPE long variables
+     */
+
+    /*
+     *long value = 0x7FFFFFFF * 0x0000FFFF;
+     *printf("expression value: %ld\n", value);
+     */
+
 
     /*FLOAT*/
 
