b2f8aad08549707ea458f0a33a5963508723e06b
maximilian1064 <lei.yan@rwth-aachen.de>
2016-11-17 21:28:40 +0100

    exercise 3-4, integer to string conversion, correct behavior for INT_MIN;  exercise 2-9, fast counting of 1-bit in integer using x&=(x-1)

---
+++ textbook-assignments/chap-2/e2-9.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/*
+ *x&(x-1) deletes the rightmost 1-bit of x
+ *REASON: x: [left bits][rightmost 1-bit][0-bits]
+ *        x-1: x+0xFFFFFFFF
+ *           [left bits][   0-bit       ][1-bits]
+ *        x&(x-1):
+ *           [left bits][   0-bit       ][0-bits]
+ */
+
+int bitcount (int x)
+{
+    int b;
+    for(b=0; x||0; ++b) { x &= x-1; }
+    return b;
+}
+
+int main()
+{
+    int test_set[5] = { 0xF5F4F59F,
+                        0xFFFFFFFF,
+                        0xBBFBFFFF,
+                        0x00000000,
+                        0xDEADBEEF };
+
+    for(int i=0; i<5; i++)
+        printf("%8x, %2d\n", test_set[i], bitcount(test_set[i]) );
+
+
+    return 0;
+}
---
+++ textbook-assignments/chap-3/e3-4.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+void reverse(char s[])
+{
+    int i;
+    for(i=0; s[i]!='\0'; ++i)
+        ;
+    for(int j=0; j<=(i-1-j); ++j)
+    {
+        char temp = s[j];
+        s[j] = s[i-1-j];
+        s[i-1-j] = temp;
+    }
+}
+
+
+/*
+ *assume on every machine the expression x and (unsigned int)x has the same binary content
+ *
+ *use all these fucking unsigned constants to ensure machine independent behavior 
+ *actually no need for my environment!!!!!!!!!!!!!
+ */
+void itoa(int n, char s[])
+{
+    int sign;
+    unsigned int m;
+
+    /*
+     *get the two's complement of n manually, 
+     *store it as an unsigned integer,
+     *to ensure correct behavior even when 
+     *n = INT_MIN
+     */
+    if( (sign=n)<0 )
+        m = ~((unsigned int)n) + 1u; 
+    else 
+        m = n;
+
+
+    int i=0;
+    do{
+        s[i++] = m%10u + 48u;
+        m /= 10u;
+    }while(m!=0u);
+    
+    if(sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+
+    reverse(s);
+}
+
+
+int main()
+{
+    int test_integer[5] = { 0x80000000,
+                            128,
+                            0x7FFFFFFF,
+                            -77900909,
+                            0 };
+    for(int i=0; i<5; ++i)
+    {
+        char s[20];
+        itoa(test_integer[i], s);
+        printf("%d,  %s\n", test_integer[i], s);
+    }
+
+
+    return 0;
+}
